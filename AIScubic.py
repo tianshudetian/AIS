3630a4bee44f7cdab6f0394259ab94103f400c8b
tianshudetian <tianshudetian@gmail.com>
2021-03-05 21:09:23 +0800

    add data

--- AIScubic.py
+++ AIScubic.py
@@ -21,20 +21,18 @@ def cubic(x_list,y_list):
 
 or_df = pd.read_csv(r'/home/mty/data/dynamic/20181001.csv')
 df = or_df.iloc[:10000, :]
-df.sort_values(by='timestamp', inplace=True)
+# df.sort_values(by='timestamp', inplace=True)
 latRange = [29.55, 30.1]
 lonRange = [121.9, 122.45]
 new_df = df.loc[(df['SOG'] >= 2.0) & (df['lon'] >= lonRange[0]) & (df['lon'] <= lonRange[-1]) & (df['lat'] >= latRange[0]) &
                        (df['lat'] <= latRange[-1])]
 MMSIs = new_df['MMSI'].unique()
+print('begin!!!')
 for MMSI in MMSIs:
+    print('MMSI: '+str(MMSI))
     tem = new_df[new_df['MMSI'].isin([MMSI])]
     temp = tem.drop_duplicates('timestamp', keep='first')
     time = temp['timestamp'].values
-    for c, v in enumerate(time):
-        if c >= 1:
-            if v - time[c - 1] == 0.0:
-                print('1')
     Indexs = diff_over_threshold(time)
     Indexs.insert(0, 0)
     Indexs.append(len(time))
@@ -52,6 +50,7 @@ for MMSI in MMSIs:
                     input_y = temp[str_name].values[index1:index2]
                     x_new, y_new = cubic(input_x, input_y)
                     new_temp[str_name] = y_new
+                    print('j: '+str(j))
                     if j == 0:
                         new_temp['timestamp'] = x_new
                         new_temp['Length'] = temp['length'].iloc[0]
