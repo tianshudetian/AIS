f459f64b49442be88b1d03bf7cad72cb6c8f5307
tianshudetian <tianshudetian@gmail.com>
2021-03-08 18:46:22 +0800

    data

--- mytest.py
+++ mytest.py
@@ -10,6 +10,25 @@ def heaviside(value,threshold=0.8):
     else:
         return 0
 
+def k_distribute_display(K_set):
+    k_distribute = np.zeros(int(K_set.max()))
+    for i in np.arange(0, K_set.max(), 1):
+        for K in K_set:
+            if K == i + 1:
+                k_distribute[int(i)] = k_distribute[int(i)] + 1
+
+    fig = plt.figure(figsize=(8, 5))
+    ax = fig.add_subplot(111)
+    x = np.arange(1, len(k_distribute) + 1, 1)
+    ax.scatter(x, k_distribute, marker='*', c='red')
+    labels = ax.get_xticklabels() + ax.get_yticklabels()
+    [label.set_fontname('Times New Roman') for label in labels]
+    ax.set_xlabel('K', fontsize=17, fontname='Times New Roman')
+    ax.set_ylabel('The number of nodes', fontsize=17, fontname='Times New Roman')
+    plt.tick_params(labelsize=16)
+    plt.grid(linestyle='--')
+
+
 input_file = r'/home/mty/conflictNUM.csv'
 df = pd.read_csv(input_file)
 origin_info = list(df.iloc[:, 1])
@@ -30,5 +49,5 @@ for index1, vector1 in enumerate(reconstruct_list):
             except:
                 pass
 K_set = np.array(k_set)
-print(K_set)
-np.save('K_set.npy', K_set)
+# print(K_set)
+# np.save('K_set.npy', K_set)
--- network.py
+++ network.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+
 K_set = np.load('K_set.npy')
 k_distribute = np.zeros(int(K_set.max()))
 for i in np.arange(0, K_set.max(), 1):
@@ -7,13 +8,15 @@ for i in np.arange(0, K_set.max(), 1):
         if K == i+1:
             k_distribute[int(i)] = k_distribute[int(i)] + 1
 
-fig = plt.figure(figsize=(5, 5))
+fig = plt.figure(figsize=(8, 5))
 ax = fig.add_subplot(111)
 x = np.arange(1, len(k_distribute)+1, 1)
 ax.scatter(x, k_distribute, marker='*', c='red')
 labels = ax.get_xticklabels() + ax.get_yticklabels()
 [label.set_fontname('Times New Roman') for label in labels]
-ax.set_xlabel('K',fontsize=14)
-ax.set_ylabel('Sum of the Squared Errors (SSE)',fontsize=14)
+ax.set_xlabel('K',fontsize=17,fontname = 'Times New Roman')
+ax.set_ylabel('The number of nodes',fontsize=17,fontname = 'Times New Roman')
+plt.tick_params(labelsize=16)
+plt.grid(linestyle='--')
 plt.show()
 breakpoint1 = 0
